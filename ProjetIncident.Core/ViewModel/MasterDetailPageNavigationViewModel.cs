f5ccf3da9c87572050afecbff2b86f796c7735e5
Adeline <adeline-dumas@hotmail.fr>
2018-03-28 17:44:46 +0200

    ajout icones

--- ProjetIncident.Core/ViewModel/MasterDetailPageNavigationViewModel.cs
+++ ProjetIncident.Core/ViewModel/MasterDetailPageNavigationViewModel.cs
@@ -12,9 +12,9 @@ namespace ProjetIncident.Core.ViewModel
         public MasterDetailPageNavigationViewModel()
         {
             menuList = new List<Model.MasterPageItem>();
-            menuList.Add(new Model.MasterPageItem() { Title = "Accueil", IconSource = "icon.png", TargetType = typeof(DescriptionIncidentView) });
-            menuList.Add(new Model.MasterPageItem() { Title = "Ajouter un incident", IconSource = "icon.png", TargetType = typeof(FormulaireIncidentView) });
-            menuList.Add(new Model.MasterPageItem() { Title = "Déconnexion", IconSource = "icon.png", TargetType = typeof(ConnexionView) });
+            menuList.Add(new Model.MasterPageItem() { Title = "Accueil", IconSource = "home.png", TargetType = typeof(DescriptionIncidentView) });
+            menuList.Add(new Model.MasterPageItem() { Title = "Ajouter un incident", IconSource = "warning.png", TargetType = typeof(FormulaireIncidentView) });
+            menuList.Add(new Model.MasterPageItem() { Title = "Déconnexion", IconSource = "logout.png", TargetType = typeof(ConnexionView) });
 
         }
 
